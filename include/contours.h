93fadc1d4fc07796c35d0900b2b7af79fc55dba1
Pu <GPu@ad.harman.com>
2017-12-08 17:57:40 +0800

    add opencv contours ok

---
+++ include/contours.h
@@ -0,0 +1,11 @@
+#ifndef __CONTOURS_H__
+#define __CONTOURS_H__
+
+#include "common.h"
+ int  cvFindContours( CvArr* image, CvMemStorage* storage, CvSeq** first_contour,
+                            int header_size CV_DEFAULT(sizeof(CvContour)),
+                            int mode CV_DEFAULT(CV_RETR_LIST),
+                            int method CV_DEFAULT(CV_CHAIN_APPROX_SIMPLE),
+                            CvPoint offset CV_DEFAULT(cvPoint(0,0)));
+
+#endif /*__CONTOURS_H__*/
---
+++ include/system.h
@@ -0,0 +1,8 @@
+
+#ifndef __SYSYTEM_H__
+#define __SYSYTEM_H__
+//#define CV_Error( code, msg )  
+//#define CV_Error_( code, args )  
+//#define CV_Assert( expr )
+
+#endif  
